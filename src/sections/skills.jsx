292a0c35f27ce762a428927068377257af24c236
CRONERT <robin.cronert@outlook.fr>
2021-01-19 15:17:49 +0100

    Reponsive laptops/desktop OK

--- src/sections/skills.jsx
+++ src/sections/skills.jsx
@@ -107,26 +107,6 @@ const Skills = () => {
                                     backContent={{ title: "Adobe XD", xp: "3" }}
                                 />
                             </SwiperSlide>
-                            {/* <SwiperSlide>
-                                <Card img="adobe_xd.png" altImg="Adobe XD logo" backColor="violet"
-                                    backContent={{ title: "Adobe XD", xp: "3" }}
-                                />
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <Card img="adobe_xd.png" altImg="Adobe XD logo" backColor="violet"
-                                    backContent={{ title: "Adobe XD", xp: "3" }}
-                                />
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <Card img="adobe_xd.png" altImg="Adobe XD logo" backColor="violet"
-                                    backContent={{ title: "Adobe XD", xp: "3" }}
-                                />
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <Card img="adobe_xd.png" altImg="Adobe XD logo" backColor="violet"
-                                    backContent={{ title: "Adobe XD", xp: "3" }}
-                                />
-                            </SwiperSlide> */}
                         </Swiper>
                     </div>
                 </div>
